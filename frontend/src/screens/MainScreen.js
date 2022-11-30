083886cfeaa346a53c4ab77b16f3a56015e325e4
Shuja Ali <shuja.ali@ropstam.com>
2022-11-30 15:05:29 +0500

    Chador:

--- frontend/src/screens/MainScreen.js
+++ frontend/src/screens/MainScreen.js
@@ -95,7 +95,6 @@ const MainScreen = () => {
             </div>
           </Row>
           <h1>Featured Products</h1>
-
           <Row>
             {products.map((product) => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
